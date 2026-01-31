d226b8beea0c549fca8dd39dcc45ffae5fe6ba28
YOYBROY <ethan.j.grigg@gmail.com>
2026-02-01 04:38:58 +1100

    Bleh, getting player interaction working

--- Assets/Code/PlayerInteractions.cs
+++ Assets/Code/PlayerInteractions.cs
@@ -7,21 +7,17 @@ public class PlayerInteractions : MonoBehaviour
     //get npcs within range of the player
     //Get npc the player is facing
 
+    [SerializeField] private float visionRange = 3f;
+    [SerializeField] private float visionAngle = 20;
+    [SerializeField] private int visionConeResolution = 120;
+
     [SerializeField] private InputActionReference attackAction;
 
-
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
+    private GameObject target;
 
     void OnEnable()
     {
+        visionAngle *= Mathf.Deg2Rad;
         attackAction.action.started += Attack;
     }
 
@@ -30,8 +26,48 @@ public class PlayerInteractions : MonoBehaviour
         attackAction.action.started -= Attack;
     }
 
+    void Update()
+    {
+        SelectTarget();
+    }
+
+    void SelectTarget()
+    {
+        float Currentangle = -visionAngle / 2;
+        float angleIncrement = visionAngle / (visionConeResolution - 1);
+        float Sine;
+        float Cosine;
+        int count = 0;
+        for (int i = 0; i < visionConeResolution; i++)
+        {
+            Sine = Mathf.Sin(Currentangle);
+            Cosine = Mathf.Cos(Currentangle);
+            Vector3 RaycastDirection = (transform.forward * Cosine) + (transform.right * Sine);
+            if (Physics.Raycast(transform.position, RaycastDirection, out RaycastHit hit, visionRange))
+            {
+                if(hit.collider.gameObject.CompareTag("NPC"))
+                {
+                    target = hit.collider.gameObject;
+                    Debug.Log("Target Acquired", hit.collider.gameObject);
+                    GameEvents.current.NPCTargeted(hit.collider.gameObject);
+                    count ++;
+                    break;
+                }
+            }
+            Debug.DrawRay(transform.position, RaycastDirection * visionRange, Color.green);
+            Currentangle += angleIncrement;
+        }
+        if (count == 0)
+        {
+            target = null;
+        }
+    }
+
     void Attack(InputAction.CallbackContext obj)
     {
+        Destroy(target);
+        GameEvents.current.NPCKilled(target);
+        target = null;
         Debug.Log("Attacked");
     }
 }
